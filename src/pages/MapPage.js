3dfa7614edd65631fe09d37af6979f72970c2d87
Tal Franco <115574200+TalFranco@users.noreply.github.com>
2023-05-24 14:59:19 +0300

    Update MapPage.js

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import axios from 'axios';
+
 import { Helmet } from 'react-helmet-async';
-import { Grid, Button, Container, Stack, Typography } from '@mui/material';
+import { Grid, Button, Container, Stack, Typography, getTableSortLabelUtilityClass } from '@mui/material';
 import Iconify from '../components/iconify';
 
 const MapPage = (props) => {
@@ -11,7 +13,7 @@ const MapPage = (props) => {
   };
 
   const [currentLocation, setCurrentLocation] = useState(null);
-
+  const [stores, setstores] = useState([]);
   useEffect(() => {
     // Get the user's current location
     if (navigator.geolocation) {
@@ -20,7 +22,19 @@ const MapPage = (props) => {
         setCurrentLocation({ lat: latitude, lng: longitude });
       });
     }
+    GetStores();
   }, []);
+  function GetStores() {
+    axios
+      .get(`https://proj.ruppin.ac.il/cgroup31/test2/tar2/api/Stores/Get`)
+      .then((res) => {
+        console.log(res.data);
+        setstores(res.data);
+      })
+      .catch((err) => {
+        console.log('err in GetStores', err);
+      });
+  }
 
   return (
     <>
@@ -29,15 +43,36 @@ const MapPage = (props) => {
       </Helmet>
       <Container>
         <Stack direction="column" spacing={2}>
-          {currentLocation && (
-            <Map
-              google={props.google}
-              zoom={12}
-              style={mapStyles}
-              initialCenter={currentLocation}
-              language="he"
+          {currentLocation &&stores&& (
+            <Map language="hebrew" google={props.google} zoom={15} style={mapStyles} initialCenter={currentLocation}>
+              {currentLocation && (
+                <Marker
+                  position={currentLocation}
+                  icon={{
+                    url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                  }}
+                  title="המיקום שלי"
+                />
+              )}
+              {/* {stores.map((store, index) => {
+                const [latitude, longitude] = store.address_coordinates.split(', ');
+                const parsedLatitude = parseFloat(latitude);
+                const parsedLongitude = parseFloat(longitude);
 
-            />
+                if (isNaN(parsedLatitude) || isNaN(parsedLongitude)) {
+                  // Handle the case when the coordinates are invalid
+                  console.log('Invalid coordinates for store:', store);
+                } else {
+                  return (
+                    <Marker
+                      key={index}
+                      position={{ lat: parsedLatitude, lng: parsedLongitude }}
+                      onClick={() => handleMarkerPress(store)}
+                    />
+                  );
+                }
+              })} */}
+            </Map>
           )}
         </Stack>
       </Container>
